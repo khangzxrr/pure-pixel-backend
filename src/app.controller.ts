3018a4bc69b312e71a6f78f46fdd6ff602411287
khangzxrr <khangzxrr@gmail.com>
2024-07-18 00:58:53 +0700

    add cache manager, prisma services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from '@prisma/client';
+import { UserService } from './services/user.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  getAllUsers(): Promise<User[]> {
+    return this.userService.users({});
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { PrismaService } from './prisma.service';
+import { UserService } from './services/user.service';
+
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [UserService, AppService, PrismaService]
 })
-export class AppModule {}
+export class AppModule { }
