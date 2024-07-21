6e3183feffa4eccb9778448cc93dda80224285df
khangzxrr <khangzxrr@gmail.com>
2024-07-22 00:33:27 +0700

    add example of supertokens authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, UseGuards, UseInterceptors, Request, Session } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from '@prisma/client';
 import { UserService } from './services/user.service';
 import { CacheInterceptor } from '@nestjs/cache-manager';
-
+import { SessionContainer } from 'supertokens-node/recipe/session';
+import { AuthGuard } from './auth/auth.guard';
+import { Record } from '@prisma/client/runtime/library';
+import { SessionRequest } from 'supertokens-node/framework/express';
+import supertokens from "supertokens-node";
 @Controller()
 @UseInterceptors(CacheInterceptor)
 export class AppController {
@@ -20,5 +24,31 @@ export class AppController {
   @Get('users')
   getAllUsers(): Promise<User[]> {
     return this.userService.users({});
+  }
+
+  @Get('/user/info')
+  @UseGuards(new AuthGuard())
+  async getUserInfo(@Request() req: SessionRequest, @Session() session: SessionContainer)
+    : Promise<Record<string, unknown>> {
+
+    let userId = session.getUserId();
+
+    let userInfo = supertokens.getUser(userId);
+
+
+    return userInfo
+  }
+
+
+  @Get('/test')
+  @UseGuards(new AuthGuard())
+  getSessionInfo(
+    @Session() session: SessionContainer,
+  ): Record<string, unknown> {
+    return {
+      sessionHandle: session.getHandle(),
+      userId: session.getUserId(),
+      accessTokenPayload: session.getAccessTokenPayload(),
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,26 @@ import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { PrismaService } from './prisma.service';
 import { UserService } from './services/user.service';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [
+    CacheModule.register(),
+    AuthModule.forRoot({
+      // https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+      connectionURI: "https://try.supertokens.com",
+      apiKey: process.env.SUPERTOKENS_API_KEYS,
+      appInfo: {
+        // Learn more about this on https://supertokens.com/docs/passwordless/appinfo
+        appName: "PurePixel",
+        apiDomain: "http://localhost:3000",
+        websiteDomain: "http://localhost:3006",
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth"
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [UserService, AppService, PrismaService]
 })
