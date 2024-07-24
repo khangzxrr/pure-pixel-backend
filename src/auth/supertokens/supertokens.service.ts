48dd8c8cf2cc517897b3474b674f4ac45c52a8fe
khangzxrr <khangzxrr@gmail.com>
2024-07-24 20:29:14 +0700

    add 3rd provider authenticate

--- src/auth/supertokens/supertokens.service.ts
+++ src/auth/supertokens/supertokens.service.ts
@@ -1,11 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
 import supertokens from "supertokens-node";
-import Session from 'supertokens-node/recipe/session';
-import Passwordless from 'supertokens-node/recipe/passwordless';
-import Dashboard from "supertokens-node/recipe/dashboard";
-import UserRoles from "supertokens-node/recipe/userroles";
 import { ConfigInjectionToken, AuthModuleConfig } from "../config.interface";
-
+import ThirdParty from "supertokens-node/recipe/thirdparty"
+import EmailPassword from "supertokens-node/recipe/emailpassword"
+import Session from 'supertokens-node/recipe/session';
 @Injectable()
 export class SupertokensService {
   constructor(@Inject(ConfigInjectionToken) private config: AuthModuleConfig) {
@@ -15,15 +13,48 @@ export class SupertokensService {
         connectionURI: config.connectionURI,
         apiKey: config.apiKey,
       },
-      recipeList: [
-        Passwordless.init({
-          flowType: "MAGIC_LINK",
-          contactMethod: "EMAIL_OR_PHONE"
-        }),
-        Session.init(),
-        Dashboard.init(),
-        UserRoles.init(),
-      ],
+      recipeList:
+        [
+          EmailPassword.init(),
+          ThirdParty.init({
+            // We have provided you with development keys which you can use for testing.
+            // IMPORTANT: Please replace them with your own OAuth keys for production use.
+            signInAndUpFeature: {
+              providers: [{
+                config: {
+                  thirdPartyId: "google",
+                  clients: [{
+                    clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+                    clientSecret: "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"
+                  }]
+                }
+              }, {
+                config: {
+                  thirdPartyId: "github",
+                  clients: [{
+                    clientId: "467101b197249757c71f",
+                    clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd"
+                  }]
+                }
+              }, {
+                config: {
+                  thirdPartyId: "apple",
+                  clients: [{
+                    clientId: "4398792-io.supertokens.example.service",
+                    additionalConfig: {
+                      keyId: "7M48Y4RYDL",
+                      privateKey:
+                        "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+                      teamId: "YWQCXGJRJL",
+                    }
+                  }]
+                }
+              }],
+            }
+          }),
+          Session.init(),
+          // ...
+        ]
     });
   }
 }
