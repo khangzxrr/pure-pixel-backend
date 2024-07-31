7bec2465d664838ad557dd5c041a5a73e0cce061
khangzxrr <khangzxrr@gmail.com>
2024-07-31 11:41:05 +0700

    add todo

--- src/user/queries/find-users/find-users.query-handler.ts
+++ src/user/queries/find-users/find-users.query-handler.ts
@@ -1,27 +1,19 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { FindUsersQuery } from "./find-users.query";
 import { PrismaService } from "src/prisma.service";
-import { Ok, Result } from "oxide.ts";
-import { User } from "@prisma/client";
 
 @QueryHandler(FindUsersQuery)
 export class FindUsersQueryHandler implements IQueryHandler {
   constructor(private prisma: PrismaService) { }
 
-  async execute(query: FindUsersQuery): Promise<Result<User[], Error>> {
-
-    const result = await this.prisma.user.findMany({
-      take: query.limit,
-      skip: query.offset,
-      where: {
-        email: {
-          contains: query.email,
-        },
-      },
-    });
-
-
-    return Ok(result);
+  //TODO: inject prisma then return query result
+  execute(query: FindUsersQuery): Promise<Result> {
+    throw new Error("Method not implemented.");
   }
 
+
 }
+
+
+
+
