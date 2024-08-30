cd2bb2b41df731e50a5fe3efbe9c512042ffdb7d
khangzxrr <khangzxrr@gmail.com>
2024-08-31 00:06:16 +0700

    remove dbmodule

--- src/authen/authen.module.ts
+++ src/authen/authen.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'src/database/database.module';
 import { KeycloakConnectModule } from 'nest-keycloak-connect';
 import { KeycloakConfigService } from 'src/customConfig/services/keycloak-config.service';
 import { CustomConfigModule } from 'src/customConfig/custom-config.module';
