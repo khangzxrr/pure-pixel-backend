ac65114e21db76b31012303b4476c81d34e2146c
khangzxrr <khangzxrr@gmail.com>
2024-09-01 15:11:16 +0700

    add photo repository

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { UserRepository } from './repositories/user.repository';
+import { PhotoRepository } from './repositories/photo.repository';
 @Module({
-  providers: [PrismaService, UserRepository],
-  exports: [PrismaService, UserRepository],
+  providers: [PrismaService, UserRepository, PhotoRepository],
+  exports: [PrismaService, UserRepository, PhotoRepository],
 })
 export class DatabaseModule {}
---
+++ src/database/repositories/photo.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PhotoVisibility } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class PhotoRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllByVisibility(visibilityStr: string) {
+    let visibility: PhotoVisibility = PhotoVisibility.PUBLIC;
+
+    if (visibilityStr == PhotoVisibility.PRIVATE) {
+      visibility = PhotoVisibility.PRIVATE;
+    } else if (visibilityStr == PhotoVisibility.SHARE_LINK) {
+      visibility = PhotoVisibility.SHARE_LINK;
+    }
+
+    return this.prisma.photo.findMany({
+      where: {
+        visibility,
+      },
+    });
+  }
+}
