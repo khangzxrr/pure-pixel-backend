54235e39f5e9d5642e43e36e26219d2a50a9e943
khangzxrr <khangzxrr@gmail.com>
2024-09-03 12:36:58 +0700

    add new method of photo repository

--- src/database/repositories/photo.repository.ts
+++ src/database/repositories/photo.repository.ts
@@ -1,10 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import { PhotoVisibility } from '@prisma/client';
+import { SignedUpload } from 'src/photo/dtos/presigned-upload-url.response.dto';
+import { Photo } from 'src/photo/entities/photo.entity';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class PhotoRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createTemporaryPhotos(userId: string, signedUploads: SignedUpload[]) {
+    //TODO: catch exception when category is not found
+    const category = await this.prisma.category.findFirst({
+      select: {
+        id: true,
+      },
+    });
+
+    const photos = signedUploads.map((u) => {
+      const photo = new Photo();
+      photo.photographerId = userId;
+      photo.originalPhotoUrl = u.storageObject;
+      photo.categoryId = category.id;
+      photo.location = '';
+      photo.photoType = 'RAW';
+      photo.watermarkThumbnailPhotoUrl = '';
+      photo.thumbnailPhotoUrl = '';
+      photo.watermarkPhotoUrl = '';
+      photo.description = '';
+      photo.captureTime = new Date();
+      photo.colorGrading = {};
+      photo.exif = {};
+      photo.showExif = false;
+      photo.watermark = false;
+      photo.visibility = 'PRIVATE';
+      photo.status = 'PENDING';
+      photo.photoTags = [];
+
+      return photo;
+    });
+
+    return this.prisma.photo.createManyAndReturn({
+      data: photos,
+
+      select: {
+        id: true,
+        originalPhotoUrl: true,
+      },
+    });
+  }
 
   async getPhotoById(id: string) {
     return this.prisma.photo.findUnique({
