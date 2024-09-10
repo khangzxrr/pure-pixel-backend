d3a33d654cdce397463071121a186d49bdd52ada
khangzxrr <khangzxrr@gmail.com>
2024-09-10 17:12:22 +0700

    get user info API summary

--- src/user/controllers/me.controller.ts
+++ src/user/controllers/me.controller.ts
@@ -4,11 +4,15 @@ import { KeycloakRoleGuard } from 'src/authen/guards/KeycloakRoleGuard.guard';
 import { UserService } from '../services/user.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserFilterDto } from '../dto/user-filter.dto';
+import { UpgradeOrderService } from 'src/upgrade/services/upgrade-order.service';
 
 @Controller('me')
 @ApiTags('user')
 export class MeController {
-  constructor(@Inject() private readonly userService: UserService) {}
+  constructor(
+    @Inject() private readonly userService: UserService,
+    @Inject() private readonly upgradeOrderService: UpgradeOrderService,
+  ) {}
 
   @ApiOperation({
     summary: 'get user info',
@@ -25,5 +29,16 @@ export class MeController {
     userFilterDto.id = user.sub;
 
     return await this.userService.findOne(userFilterDto);
+  }
+
+  @ApiOperation({
+    summary: 'get user current upgrade package',
+  })
+  @Get('/current-upgrade-package')
+  @UseGuards(AuthGuard, KeycloakRoleGuard)
+  async getMeCurrentUpgradePackage(@AuthenticatedUser() user) {
+    return await this.upgradeOrderService.findActiveUpgradePackageOrderByUserId(
+      user.sub,
+    );
   }
 }
