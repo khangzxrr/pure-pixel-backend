f0115b620ccb54f88915bf6f89d0d6a0176a4fdf
khangzxrr <khangzxrr@gmail.com>
2024-09-13 15:58:16 +0700

    remove cache add photo constant

---
+++ src/photo/constants/photo.constant.ts
@@ -0,0 +1,3 @@
+export class PhotoConstant {
+  static PHOTO_PROCESS_QUEUE = 'PHOTO_PROCESS_QUEUE';
+}
--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { StorageController } from './controllers/storage.controller';
-import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-yet';
 import { StorageService } from './services/storage.service';
 import { HttpModule } from '@nestjs/axios';
 import { SftpService } from './services/sftp.service';
@@ -11,10 +9,6 @@ import { SftpService } from './services/sftp.service';
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
-    }),
-    CacheModule.register({
-      store: redisStore,
-      url: process.env.REDIS_URL,
     }),
   ],
   exports: [StorageService, SftpService],
