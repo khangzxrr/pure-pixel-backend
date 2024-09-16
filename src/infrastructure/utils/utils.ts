ad5f935f24825c3268e9d5d7fef61ebdfc258692
khangzxrr <khangzxrr@gmail.com>
2024-09-16 19:40:30 +0700

    add keycloak utils

--- src/infrastructure/utils/utils.ts
+++ src/infrastructure/utils/utils.ts
@@ -1,3 +1,12 @@
+import { ContextType, ExecutionContext } from '@nestjs/common';
+import KeycloakConnect from 'keycloak-connect';
+import {
+  KeycloakMultiTenantService,
+  KeycloakConnectConfig,
+} from 'nest-keycloak-connect';
+
+type GqlContextType = 'graphql' | ContextType;
+
 export class Utils {
   static randomString(length: number) {
     let result = '';
@@ -15,3 +24,59 @@ export class Utils {
 
   static regexFileExtension = /(?:\.([^.]+))?$/;
 }
+
+export const useKeycloak = async (
+  request: any,
+  jwt: string,
+  singleTenant: KeycloakConnect.Keycloak,
+  multiTenant: KeycloakMultiTenantService,
+  opts: KeycloakConnectConfig,
+): Promise<KeycloakConnect.Keycloak> => {
+  if (opts.multiTenant && opts.multiTenant.realmResolver) {
+    const resolvedRealm = opts.multiTenant.realmResolver(request);
+    const realm =
+      resolvedRealm instanceof Promise ? await resolvedRealm : resolvedRealm;
+    return await multiTenant.get(realm, request);
+  } else if (!opts.realm) {
+    const payload = parseToken(jwt);
+    const issuerRealm = payload.iss.split('/').pop();
+    return await multiTenant.get(issuerRealm, request);
+  }
+  return singleTenant;
+};
+
+export const extractRequest = (context: ExecutionContext): [any, any] => {
+  let request: any, response: any;
+
+  // Check if request is coming from graphql or http
+  if (context.getType() === 'http') {
+    // http request
+    const httpContext = context.switchToHttp();
+
+    request = httpContext.getRequest();
+    response = httpContext.getResponse();
+  } else if (context.getType<GqlContextType>() === 'graphql') {
+    let gql: any;
+    // Check if graphql is installed
+    try {
+      gql = require('@nestjs/graphql');
+    } catch (er) {
+      throw new Error('@nestjs/graphql is not installed, cannot proceed');
+    }
+
+    // graphql request
+    const gqlContext = gql.GqlExecutionContext.create(context).getContext();
+
+    request = gqlContext.req;
+    response = gqlContext.res;
+  } else if (context.getType() === 'ws') {
+    request = context.getArgs()[0];
+  }
+
+  return [request, response];
+};
+
+export const parseToken = (token: string): any => {
+  const parts = token.split('.');
+  return JSON.parse(Buffer.from(parts[1], 'base64').toString());
+};
