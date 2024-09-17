72169aa181f9ae78ea487ab2b68dcc8011731200
khangzxrr <khangzxrr@gmail.com>
2024-09-17 21:43:10 +0700

    add origin and class transform pipe

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { json, urlencoded } from 'express';
 
@@ -19,11 +19,13 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      // whitelist: true,
     }),
   );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.enableCors({
-    origin: ['http://localhost:3000', '*'],
+    origin: ['http://localhost:3000', 'https://purepixel.io.vn', '*'],
     allowedHeaders: ['content-type', 'Authorization'],
     credentials: true,
   });
