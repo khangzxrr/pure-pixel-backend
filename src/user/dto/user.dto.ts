4785c12f876e17652883f5119b8f134c1519b946
khangzxrr <khangzxrr@gmail.com>
2024-09-17 21:42:55 +0700

    rename user to UserEntity

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,5 +1,5 @@
-import { User } from '../entities/user.entity';
+import { UserEntity } from '../entities/user.entity';
 
 export class UserDto {
-  constructor(User: User) {}
+  constructor(User: UserEntity) {}
 }
