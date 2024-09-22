085d23e4572028876de4da9f61ca54986e7fecae
khangzxrr <khangzxrr@gmail.com>
2024-09-22 16:43:40 +0700

    remove user controller, add mapping for meDto, role base mapping for findOne user service

--- src/user/controllers/me.controller.ts
+++ src/user/controllers/me.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Inject, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
 import { AuthenticatedUser, AuthGuard } from 'nest-keycloak-connect';
 import { KeycloakRoleGuard } from 'src/authen/guards/KeycloakRoleGuard.guard';
 import { UserService } from '../services/user.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserFilterDto } from '../dto/user-filter.dto';
 import { UpgradeOrderService } from 'src/upgrade/services/upgrade-order.service';
+import { ParsedUserDto } from '../dto/parsed-user.dto';
+import { MeDto } from '../dto/me.dto';
 
 @Controller('me')
 @ApiTags('user')
@@ -17,13 +19,16 @@ export class MeController {
   @ApiOperation({
     summary: 'get user info',
   })
+  @ApiOkResponse({
+    type: MeDto,
+  })
   @Get()
   @UseGuards(AuthGuard, KeycloakRoleGuard)
   async getMeInfo(
     @AuthenticatedUser()
-    user: any,
-    @Query() userFilterDto: UserFilterDto,
+    user: ParsedUserDto,
   ) {
+    const userFilterDto = new UserFilterDto();
     userFilterDto.id = user.sub;
 
     return await this.userService.findOne(userFilterDto);
