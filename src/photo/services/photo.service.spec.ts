230dd468fa16de1338fdadc0aab96872316348ea
khangzxrr <khangzxrr@gmail.com>
2024-09-23 01:15:59 +0700

    add getPresignedUploadUrl test

--- src/photo/services/photo.service.spec.ts
+++ src/photo/services/photo.service.spec.ts
@@ -1,24 +1,81 @@
 import { Test } from '@nestjs/testing';
 import { PhotoService } from './photo.service';
-import { DatabaseModule } from 'src/database/database.module';
 import { StorageModule } from 'src/storage/storage.module';
 import { QueueModule } from 'src/queue/queue.module';
+import { PresignedUploadUrlRequest } from '../dtos/presigned-upload-url.request';
+import { FileIsNotValidException } from '../exceptions/file-is-not-valid.exception';
+import { PhotoRepository } from 'src/database/repositories/photo.repository';
+import { SignedUpload } from '../dtos/presigned-upload-url.response.dto';
 
 describe('PhotoService', () => {
   let photoService: PhotoService;
+  const photoRepository = {
+    //lazy mock, write full object if required
+    createTemporaryPhotos: (userId: string, signedUploads: SignedUpload[]) => {
+      const result = signedUploads.map((su) => {
+        return {
+          id: '90ff5312-51e1-456b-ad05-67e175ac532c',
+          originalPhotoUrl: su.storageObject,
+        };
+      });
+      return Promise.resolve(result);
+    },
+  };
+
+  const userId = '6db1c6a1-f4ab-4ecb-a489-9285ebb53135';
+  const presignedUploadUrlRequest = new PresignedUploadUrlRequest();
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [DatabaseModule, StorageModule, QueueModule],
-      providers: [PhotoService],
-    }).compile();
+      imports: [StorageModule, QueueModule],
+      providers: [PhotoService, PhotoRepository],
+    })
+      .overrideProvider(PhotoRepository)
+      .useValue(photoRepository)
+      .compile();
 
     photoService = moduleRef.get<PhotoService>(PhotoService);
   });
 
-  describe('should be defined', () => {
-    it('should define service', async () => {
-      expect(photoService).toBeDefined();
+  it('should define service', async () => {
+    expect(photoService).toBeDefined();
+  });
+
+  describe('getPresignedUploadUrl', () => {
+    it('should throw FileIsNotValidException when extension is empty', () => {
+      presignedUploadUrlRequest.filenames = ['test'];
+
+      expect(
+        async () =>
+          await photoService.getPresignedUploadUrl(
+            userId,
+            presignedUploadUrlRequest,
+          ),
+      ).rejects.toThrow(FileIsNotValidException);
+    });
+
+    it('should throw FileIsNotValidException when extension is not valid', async () => {
+      presignedUploadUrlRequest.filenames = ['test.abc'];
+
+      expect(
+        async () =>
+          await photoService.getPresignedUploadUrl(
+            userId,
+            presignedUploadUrlRequest,
+          ),
+      ).rejects.toThrow(FileIsNotValidException);
+    });
+
+    it('should return correct SignedUpload when filename is valid', async () => {
+      presignedUploadUrlRequest.filenames = ['test.jpg'];
+
+      const result = await photoService.getPresignedUploadUrl(
+        userId,
+        presignedUploadUrlRequest,
+      );
+
+      expect(result).toHaveProperty('signedUploads');
+      expect(result.signedUploads[0].uploadUrl.length > 0).toBeTruthy();
     });
   });
 });
