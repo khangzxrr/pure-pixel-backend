09b851b20f1c28b73036c9b92d010fdc5fcafc24
khangzxrr <khangzxrr@gmail.com>
2024-09-27 16:41:56 +0700

    throwing wrong exception name... @@!

--- src/photo/exceptions/share-photo-url-is-empty.exception.ts
+++ src/photo/exceptions/share-photo-url-is-empty.exception.ts
@@ -1,8 +1,7 @@
 import { BadRequestException } from '@nestjs/common';
-import { ShareStatusIsNotReadyException } from './share-status-is-not-ready.exception';
 
 export class SharePhotoUrlIsEmptyException extends BadRequestException {
   constructor() {
-    super(ShareStatusIsNotReadyException.name);
+    super(SharePhotoUrlIsEmptyException.name);
   }
 }
