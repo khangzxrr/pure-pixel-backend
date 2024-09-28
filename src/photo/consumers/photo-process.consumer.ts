3b8a20d0dd2bb91d3406c48aaafcb62525f83e3b
khangzxrr <khangzxrr@gmail.com>
2024-09-29 03:25:15 +0700

    add watermark queue and remove watermark method from process consumer

--- src/photo/consumers/photo-process.consumer.ts
+++ src/photo/consumers/photo-process.consumer.ts
@@ -6,7 +6,6 @@ import { PhotoRepository } from 'src/database/repositories/photo.repository';
 import { PhotoProcessService } from '../services/photo-process.service';
 import { PhotoGateway } from '../gateways/socket.io.gateway';
 import { PhotoService } from '../services/photo.service';
-import { GenerateWatermarkRequestDto } from '../dtos/generate-watermark.request.dto';
 import { ProcessPhotosRequest } from '../dtos/process-images.request.dto';
 import { DatabaseService } from 'src/database/database.service';
 import { UserRepository } from 'src/database/repositories/user.repository';
@@ -24,7 +23,6 @@ export class PhotoProcessConsumer extends WorkerHost {
     @Inject() private readonly photoRepository: PhotoRepository,
     @Inject() private readonly userRepository: UserRepository,
     @Inject() private readonly photoProcessService: PhotoProcessService,
-    @Inject() private readonly photoService: PhotoService,
     @Inject() private readonly databaseService: DatabaseService,
   ) {
     super();
@@ -39,35 +37,12 @@ export class PhotoProcessConsumer extends WorkerHost {
             job.data.processPhotosRequest,
           );
           break;
-        case PhotoConstant.GENERATE_WATERMARK_JOB:
-          await this.processWatermark(
-            job.data.userId,
-            job.data.generateWatermarkRequest,
-          );
-          break;
       }
     } catch (e) {
       console.log(e);
       this.logger.error(e);
       throw new Error(); //perform retry
     }
-  }
-
-  async processWatermark(
-    userId: string,
-    generateWatermarkRequest: GenerateWatermarkRequestDto,
-  ) {
-    this.logger.log(`generate watermark`);
-    this.logger.log(generateWatermarkRequest);
-
-    await this.generateWatermark(generateWatermarkRequest);
-
-    const photo = await this.photoService.getSignedPhotoById(
-      userId,
-      generateWatermarkRequest.photoId,
-    );
-
-    await this.photoGateway.sendFinishWatermarkEventToUserId(userId, photo);
   }
 
   async generateSharePayload(sharp: Sharp, photo: Photo) {
@@ -104,56 +79,11 @@ export class PhotoProcessConsumer extends WorkerHost {
     );
   }
 
-  async generateWatermark(
-    generateWatermarkRequest: GenerateWatermarkRequestDto,
-  ) {
-    const photo = await this.photoRepository.getPhotoById(
-      generateWatermarkRequest.photoId,
-    );
-
-    const sharp = await this.photoProcessService.sharpInitFromObjectKey(
-      photo.originalPhotoUrl,
-    );
-
-    const watermark = await this.photoProcessService.makeWatermark(
-      sharp,
-      generateWatermarkRequest.text,
-    );
-
-    const watermarkBuffer = await watermark.toBuffer();
-    photo.watermarkPhotoUrl = `watermark/${photo.originalPhotoUrl}`;
-    await this.photoProcessService.uploadFromBuffer(
-      photo.watermarkPhotoUrl,
-      watermarkBuffer,
-    );
-
-    //make a copy from buffer DO NOT DIRECT USE watermark: sharp
-    //or it will throw composite image not valid size
-    const watermarkFromBuffer =
-      await this.photoProcessService.sharpInitFromBuffer(watermarkBuffer);
-    const watermarkThumbnail =
-      await this.photoProcessService.makeThumbnail(watermarkFromBuffer);
-
-    const watermarkThumbnailBuffer = await watermarkThumbnail.toBuffer();
-    photo.watermarkThumbnailPhotoUrl = `thumbnail/${photo.originalPhotoUrl}`;
-    await this.photoProcessService.uploadFromBuffer(
-      photo.watermarkThumbnailPhotoUrl,
-      watermarkThumbnailBuffer,
-    );
-
-    await this.photoRepository.update(photo);
-  }
-
   async processPhoto(userId: string, processRequest: ProcessPhotosRequest) {
     this.logger.log(`process photos`);
     this.logger.log(processRequest);
 
-    const processResult = await this.convertAndEmitProcessEvents(
-      userId,
-      processRequest,
-    );
-
-    await this.generateSharePayload(processResult.sharp, processResult.photo);
+    await this.convertAndEmitProcessEvents(userId, processRequest);
   }
 
   async convertAndEmitProcessEvents(
@@ -227,10 +157,5 @@ export class PhotoProcessConsumer extends WorkerHost {
     ]);
 
     await this.photoGateway.sendFinishProcessPhotoEventToUserId(userId, photo);
-
-    return {
-      photo,
-      sharp,
-    };
   }
 }
--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -29,6 +29,12 @@ import { UpgradeConstant } from 'src/upgrade/constants/upgrade.constant';
       useExisting: BullMqQueueRegisterService,
       inject: [CustomConfigModule],
     }),
+    BullModule.registerQueueAsync({
+      imports: [CustomConfigModule],
+      name: PhotoConstant.PHOTO_WATERMARK_QUEUE,
+      useExisting: BullMqQueueRegisterService,
+      inject: [CustomConfigModule],
+    }),
   ],
 })
 export class QueueModule {}
