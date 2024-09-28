1fd37dc78382ccfcab4369215513b7e08ed551e7
khangzxrr <khangzxrr@gmail.com>
2024-09-28 14:38:13 +0700

    validate nested

--- src/photo/dtos/process-images.request.dto.ts
+++ src/photo/dtos/process-images.request.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { SignedUpload } from './presigned-upload-url.response.dto';
+import { IsNotEmptyObject, ValidateNested } from 'class-validator';
 
 export class ProcessPhotosRequest {
   @ApiProperty()
+  @ValidateNested()
+  @IsNotEmptyObject()
   signedUpload: SignedUpload;
 }
