50fe1349043e6aa7e9fcf3101e31c3eb54f03e81
khangzxrr <khangzxrr@gmail.com>
2024-10-15 02:33:18 +0700

    add notification queue, email notification, saving notification to DB

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './services/notification.service';
 import { NotificationController } from './controllers/notification.controller';
-import { PushNotificationConsumer } from './consumers/push-notification.consumer';
+import { NotificationConsumer } from './consumers/notification.consumer';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  providers: [NotificationService, PushNotificationConsumer],
+  imports: [
+    DatabaseModule,
+    MailerModule.forRoot({
+      transport: `smtps://${process.env.SMTP_USERNAME}:${process.env.SMTP_PASSWORD}@${process.env.SMTP_SERVER}`,
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [NotificationService, NotificationConsumer],
   exports: [NotificationService],
   controllers: [NotificationController],
 })
