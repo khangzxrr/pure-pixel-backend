6a7eaeb8e0abaf2c0f67a51fdf29b13ede293e6e
khangzxrr <khangzxrr@gmail.com>
2024-10-17 17:39:42 +0700

    add status and define findAll photoshoot package for customer

--- src/photoshoot-package/dtos/rest/photoshoot-package-find-all.request.dto.ts
+++ src/photoshoot-package/dtos/rest/photoshoot-package-find-all.request.dto.ts
@@ -1,14 +1,23 @@
-import { Prisma } from '@prisma/client';
+import { PhotoshootPackageStatus, Prisma } from '@prisma/client';
+import { Exclude } from 'class-transformer';
 import { PagingPaginatedRequestDto } from 'src/infrastructure/restful/paging-paginated.request.dto';
 
 export class PhotoshootPackageFindAllDto extends PagingPaginatedRequestDto {
+  @Exclude()
   userId?: string;
+
+  @Exclude()
+  status?: PhotoshootPackageStatus;
 
   toWhere(): Prisma.PhotoshootPackageWhereInput {
     const where: Prisma.PhotoshootPackageWhereInput = {};
 
     if (this.userId) {
       where.userId = this.userId;
+    }
+
+    if (this.status) {
+      where.status = this.status;
     }
 
     return where;
