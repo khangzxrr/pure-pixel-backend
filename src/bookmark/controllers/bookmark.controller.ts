f173af9df439365f747671cd6fcf2a9ccb97c767
khangzxrr <khangzxrr@gmail.com>
2024-11-01 01:02:35 +0700

    remove get photo in bookmark

--- src/bookmark/controllers/bookmark.controller.ts
+++ src/bookmark/controllers/bookmark.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Delete,
-  Get,
   Inject,
   Param,
   Post,
@@ -13,22 +12,12 @@ import { AuthenticatedUser, AuthGuard } from 'nest-keycloak-connect';
 import { KeycloakRoleGuard } from 'src/authen/guards/KeycloakRoleGuard.guard';
 import { ParsedUserDto } from 'src/user/dtos/parsed-user.dto';
 import { BookmarkDto } from '../dtos/bookmark.dto';
-import { ApiOkResponsePaginated } from 'src/infrastructure/decorators/paginated.response.dto';
-
-import { SignedPhotoDto } from 'src/photo/dtos/signed-photo.dto';
 
 @Controller('bookmark')
 @ApiTags('bookmark')
 @UseGuards(AuthGuard, KeycloakRoleGuard)
 export class BookmarkController {
   constructor(@Inject() private readonly bookmarkService: BookmarkService) {}
-
-  @Get('/photo')
-  @ApiOperation({
-    summary: 'find all bookmarked photos',
-  })
-  @ApiOkResponsePaginated(SignedPhotoDto)
-  async findAllBookmarkedPhoto() {}
 
   @Post(':photoId')
   @ApiOperation({
