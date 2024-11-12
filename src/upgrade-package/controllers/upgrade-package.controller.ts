4240bb6423cc6ef6a8aecad9c22c9b4a0c4263d7
khangzxrr <khangzxrr@gmail.com>
2024-11-12 22:47:35 +0700

    seperate manage upgrade package

--- src/upgrade-package/controllers/upgrade-package.controller.ts
+++ src/upgrade-package/controllers/upgrade-package.controller.ts
@@ -1,27 +1,9 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  Param,
-  Patch,
-  Post,
-  Put,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UpgradePackageDto } from '../dtos/upgrade-package.dto';
 import { ApiOkResponsePaginated } from 'src/infrastructure/decorators/paginated.response.dto';
 import { UpgradePackageFindAllDto } from '../dtos/rest/upgrade-package-find-all.request.dto';
 import { UpgradePackageService } from '../services/upgrade-package.service';
-import { CreateUpgradePackageDto } from '../dtos/rest/create-upgrade-package.request.dto';
-import { AuthGuard, Roles } from 'nest-keycloak-connect';
-import { Constants } from 'src/infrastructure/utils/constants';
-import { KeycloakRoleGuard } from 'src/authen/guards/KeycloakRoleGuard.guard';
-import { PatchUpdateUpgradePackageDto } from '../dtos/rest/patch-update-upgrade-package.request.dto';
-import { PutUpdateUpgradePackageDto } from '../dtos/rest/put-update-upgrade-package.request.dto';
 
 @Controller('upgrade-package')
 @ApiTags('upgrade-package')
@@ -37,71 +19,5 @@ export class UpgradePackageController {
   @ApiOkResponsePaginated(UpgradePackageDto)
   async findAll(@Query() findAllDto: UpgradePackageFindAllDto) {
     return await this.upgradePackageService.findAll(findAllDto);
-  }
-
-  @Delete(':id')
-  @ApiOperation({
-    summary: 'delete a upgrade package by id',
-  })
-  @ApiOkResponse({
-    type: UpgradePackageDto,
-  })
-  @UseGuards(AuthGuard, KeycloakRoleGuard)
-  @Roles({ roles: [Constants.MANAGER_ROLE] })
-  async delete(@Param('id') id: string) {
-    return await this.upgradePackageService.delete(id);
-  }
-
-  @Post()
-  @ApiOperation({
-    summary: 'create new upgrade package',
-  })
-  @ApiOkResponse({
-    type: UpgradePackageDto,
-  })
-  @UseGuards(AuthGuard, KeycloakRoleGuard)
-  @Roles({ roles: [Constants.MANAGER_ROLE] })
-  async createUpgradePackage(
-    @Body() createUpgradePackageDto: CreateUpgradePackageDto,
-  ) {
-    return await this.upgradePackageService.create(createUpgradePackageDto);
-  }
-
-  @Patch(':id')
-  @ApiOperation({
-    summary: 'update one or more fields of upgrade package by id',
-  })
-  @ApiOkResponse({
-    type: UpgradePackageDto,
-  })
-  @UseGuards(AuthGuard, KeycloakRoleGuard)
-  @Roles({ roles: [Constants.MANAGER_ROLE] })
-  async patchUpdatePackage(
-    @Param('id') id: string,
-    @Body() patchUpdateUpgradePackageDto: PatchUpdateUpgradePackageDto,
-  ) {
-    return await this.upgradePackageService.update(
-      id,
-      patchUpdateUpgradePackageDto,
-    );
-  }
-
-  @Put(':id')
-  @ApiOperation({
-    summary: 'replace all fields of upgrade package by id',
-  })
-  @ApiOkResponse({
-    type: UpgradePackageDto,
-  })
-  @UseGuards(AuthGuard, KeycloakRoleGuard)
-  @Roles({ roles: [Constants.MANAGER_ROLE] })
-  async putUpdatePackage(
-    @Param('id') id: string,
-    @Body() putUpdateUpgradePackageDto: PutUpdateUpgradePackageDto,
-  ) {
-    return await this.upgradePackageService.replace(
-      id,
-      putUpdateUpgradePackageDto,
-    );
   }
 }
--- src/upgrade-package/upgrade-package.module.ts
+++ src/upgrade-package/upgrade-package.module.ts
@@ -4,10 +4,11 @@ import { UpgradePackageController } from './controllers/upgrade-package.controll
 import { AuthenModule } from 'src/authen/authen.module';
 import { StorageModule } from 'src/storage/storage.module';
 import { UpgradePackageService } from './services/upgrade-package.service';
+import { ManageUpgradePackageController } from './controllers/manage-upgrade-package.controller';
 
 @Module({
   imports: [DatabaseModule, AuthenModule, StorageModule],
-  controllers: [UpgradePackageController],
+  controllers: [UpgradePackageController, ManageUpgradePackageController],
   providers: [UpgradePackageService],
 })
 export class UpgradePackageModule {}
