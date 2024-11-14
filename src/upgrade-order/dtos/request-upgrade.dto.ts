1dc1c12181e059cf8ad7531f80acfdf91418c710
Vo Khang <vokhang@MacBook-Pro-cua-Vo.local>
2024-11-14 17:29:04 +0700

    fix upgrade order

--- src/upgrade-order/dtos/request-upgrade.dto.ts
+++ src/upgrade-order/dtos/request-upgrade.dto.ts
@@ -1,24 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsString, Max, Min } from 'class-validator';
-import { ToBoolean } from 'src/infrastructure/transforms/to-boolean';
+import { IsNumber, IsString, Max, Min } from 'class-validator';
 
 export class RequestUpgradeDto {
-  @ApiProperty({
-    description:
-      'accept transfer days from current to new order package, this must be true if user already activate an upgrade package',
-  })
-  @ToBoolean()
-  @IsBoolean()
-  acceptTransfer: boolean;
-
-  @ApiProperty({
-    description:
-      'accept remove all previous pending upgrade order, this must be true if user has pending upgrade order',
-  })
-  @ToBoolean()
-  @IsBoolean()
-  acceptRemovePendingUpgradeOrder: boolean;
-
   @ApiProperty({
     description: 'upgrade package ID',
   })
--- src/upgrade-order/services/upgrade-order.service.ts
+++ src/upgrade-order/services/upgrade-order.service.ts
@@ -49,7 +49,7 @@ export class UpgradeOrderService {
         userId,
       );
 
-    if (activedUpgradePackage != null && !requestUpgrade.acceptTransfer) {
+    if (activedUpgradePackage != null) {
       throw new UserHasActivatedUpgradePackage();
     }
   }
@@ -69,19 +69,13 @@ export class UpgradeOrderService {
     return upgradePackage;
   }
 
-  private async checkPendingOrderAndAcceptCancelPending(
-    userId: string,
-    requestUpgrade: RequestUpgradeDto,
-  ) {
+  private async checkPendingOrderAndAcceptCancelPending(userId: string) {
     const pendingOrders =
       await this.upgradePackageOrderRepository.findManyPendingOrderByUserId(
         userId,
       );
 
-    if (
-      pendingOrders.length > 0 &&
-      !requestUpgrade.acceptRemovePendingUpgradeOrder
-    ) {
+    if (pendingOrders.length > 0) {
       throw new ExistPendingUpgradeOrderException();
     }
 
@@ -100,10 +94,8 @@ export class UpgradeOrderService {
     const upgradePackage =
       await this.checkUpgradePackageMustExist(requestUpgrade);
 
-    const pendingOrders = await this.checkPendingOrderAndAcceptCancelPending(
-      userId,
-      requestUpgrade,
-    );
+    const pendingOrders =
+      await this.checkPendingOrderAndAcceptCancelPending(userId);
 
     //TODO: handle transfer upgrade packages
 
@@ -163,14 +155,17 @@ export class UpgradeOrderService {
           newUpgradeOrder.upgradePackageHistory.id;
 
         //IMPORTANT: must using transactionId instead of serviceTransactionId
-
-        const paymentDto = await this.sepayService.generatePayment(
-          newUpgradeOrder.serviceTransaction.transactionId,
-          calculatedPrice.toNumber(),
-        );
-
-        requestUpgradeResponse.paymentUrl = paymentDto.paymentUrl;
-        requestUpgradeResponse.mockQrCode = paymentDto.mockQrCode;
+        //using direct generate payment because in transaction scope, we dont have entity yet
+        requestUpgradeResponse.paymentUrl =
+          this.sepayService.generatePaymentUrl(
+            newUpgradeOrder.serviceTransaction.transactionId,
+            calculatedPrice.toNumber(),
+          );
+        requestUpgradeResponse.mockQrCode =
+          await this.sepayService.generateMockIpnQrCode(
+            newUpgradeOrder.serviceTransaction.transactionId,
+            calculatedPrice.toNumber(),
+          );
 
         return requestUpgradeResponse;
       },
