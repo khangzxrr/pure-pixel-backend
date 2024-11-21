78b79ac5301572ffd67df31b9ae2ea7500b6338d
khangzxrr <khangzxrr@gmail.com>
2024-11-21 12:46:04 +0700

    optinal payload

--- prisma/migrations/20241121054504_optional_payload_notification/migration.sql
+++ prisma/migrations/20241121054504_optional_payload_notification/migration.sql
@@ -2,9 +2,8 @@
   Warnings:
 
   - You are about to drop the column `referenceId` on the `Notification` table. All the data in the column will be lost.
-  - Added the required column `payload` to the `Notification` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "Notification" DROP COLUMN "referenceId",
-ADD COLUMN     "payload" JSONB NOT NULL;
+ADD COLUMN     "payload" JSONB;
