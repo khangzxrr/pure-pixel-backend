7bf3aeeb001dc2e7b768ce6f3e99e7bb7497e1e3
khangzxrr <khangzxrr@gmail.com>
2024-12-02 15:41:24 +0700

    fix optional thumbnail

--- src/camera/dtos/rest/update-camera.dto.ts
+++ src/camera/dtos/rest/update-camera.dto.ts
@@ -1,6 +1,7 @@
 import { ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
 import { CameraDto } from '../camera.dto';
 import { IsFile, MemoryStoredFile } from 'nestjs-form-data';
+import { IsOptional } from 'class-validator';
 
 export class UpdateCameraDto extends OmitType(PartialType(CameraDto), [
   'createdAt',
@@ -12,6 +13,7 @@ export class UpdateCameraDto extends OmitType(PartialType(CameraDto), [
   @ApiPropertyOptional({
     type: 'file',
   })
+  @IsOptional()
   @IsFile()
   thumbnail?: MemoryStoredFile;
 }
