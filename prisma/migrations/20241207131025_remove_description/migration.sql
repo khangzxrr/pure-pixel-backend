f17f57033e2368078c472722446ce3ba8da9a348
khangzxrr <khangzxrr@gmail.com>
2024-12-07 20:15:27 +0700

    remove title, description photo selling

---
+++ prisma/migrations/20241207131025_remove_description/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `PhotoSellHistory` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PhotoSellHistory" DROP COLUMN "description";
